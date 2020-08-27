aa2458c5f75cc79996e07af1746865dc3db8a7da
Inioluwa Akinyosoye <inisoye@gmail.com>
2020-08-27 15:47:04 +0100

    update csv

--- oop.py
+++ oop.py
@@ -125,35 +125,58 @@ class Account(User):
         return str(accountNumber)
 
     # Default values make function parameters optional. The code will still run without them--hence the empty strings
-    def deposit(self, amount, comment=""):
+    # Source is empty (false) because money is assumed to be a personal deposit by default.
+    def deposit(self, amount, comment="", source=""):
+        transactionLabel = "credit"
+
+        if source:
+            transactionType = "transfer"
+            source = source.name
+        else:
+            transactionType = "deposit"
+            source = self.name
+
         self.balance += amount  # Add deposit value to balance
-        self.storeTransaction("credit", amount, comment)
+        self.storeTransaction(
+            transactionType, transactionLabel, amount, self.name, source, comment)
         print(
             f"Welldone {self.name} your deposit of ₦{amount} was sucessful. Your new balance is ₦{self.balance}.")
 
-    def withdraw(self, amount, comment=""):
-        self.balance -= amount  # Remove withdrawal value from balance
-        self.storeTransaction("debit", amount, comment)
+    def withdraw(self, amount, comment="", collector=""):
+        transactionLabel = "debit"
+
+        if collector:
+            transactionType = "transfer"
+            collector = collector.name
+        else:
+            transactionType = "withdrawal"
+            collector = self.name
+
+        self.balance += amount  # Add deposit value to balance
+        self.storeTransaction(
+            transactionType, transactionLabel, amount, self.name, collector, comment)
         print(
             f"Welldone {self.name} your deposit of ₦{amount} was sucessful. Your new balance is ₦{self.balance}.")
 
     def transfer(self, amount, recipient, comment=""):
-        self.withdraw(amount)  # Remove transfer value from sender's balance
-        recipient.deposit(amount)  # Add transfer value to recipient's balance
-        self.storeTransaction("transfer", amount, comment)
+        # Remove transfer value from sender's balance
+        self.withdraw(amount, comment, recipient)
+        recipient.deposit(amount, comment, self)  # Add transfer value to recipient's balance
+
         print(
             f"Congrats {self.name} your transfer of ₦{amount} to {recipient.name} was sucessful. Your new balance is ₦{self.balance}.")
 
-    def storeTransaction(self, type,  amount, comment, recipient="To No One"):
-        file = open("financial-statement.csv", "a")
-        file.write(f"{type}, {self.name}, {amount}, {comment}, {recipient}\n")
-        print(type, amount, comment, recipient)
+    def storeTransaction(self, transactionType, transactionLabel,  amount, source, comment, recipient="To No One"):
+        file=open("financial-statement.csv", "a")
+        file.write(
+            f"{transactionType}, {transactionLabel}, {amount}, {source}, {comment}, {recipient}\n")
+        print(transactionType, amount, comment, recipient)
 
 
-ini = Account("Ini", 23, "inisoye@gmail.com", "07089559879")
+ini=Account("Ini", 23, "inisoye@gmail.com", "07089559879")
 print(ini.accountNumber)
 ini.deposit(20000)
 ini.withdraw(3000)
 
-ayo = Account("ayo", 23, "ayosoye@gmail.com", "07089559879")
+ayo=Account("Ayo", 23, "ayosoye@gmail.com", "07089559879")
 ini.transfer(2000, ayo, "flexing")
